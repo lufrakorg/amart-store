38f2397324031ba88c2a70bd246699fec6683922
Lufrak <lufrakorg@gmail.com>
2026-09-11 11:25:10 +0200

    Delete src/data/products.ts

--- src/data/products.ts
+++
@@ -1,7 +0,0 @@
-import { getCollection } from "astro:content";
-
-const rawProducts = await getCollection("products");
-export const products = rawProducts.map((p) => ({ id: p.id, ...p.data }));
-
-/** Products featured on homepage carousel (first 6) */
-export const featuredProducts = products.slice(0, 6);
