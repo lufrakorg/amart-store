3382b59b153d036400478c48dfaacb56ab6838f1
Lufrak <lufrakorg@gmail.com>
2026-09-12 17:22:35 +0200

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
+  site: "https://amartstoresalerno.com",
+  trailingSlash: "never",
   integrations: [tailwind()],
   output: "static",
 });
